35cc767e4add7e73f3a87ced2168e0d181436604
mattlag <mattlag@gmail.com>
2016-04-30 00:00:11 -0700

    Glyph Edit and Settings pages

--- webpack.config.js
+++ webpack.config.js
@@ -2,24 +2,29 @@ var path = require('path');
 
 module.exports = {
   entry: {
-    frame: "./src/entry.js"
+    glyphedit: "./src/glyphedit/entry.js",
+    settings: "./src/settings/entry.js",
+    openproject: "./src/openproject/entry.js"
   },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: "bundle.js"
+    filename: "[name].bundle.js",
+    chunkFilename: "[id].bundle.js"
   },
   module: {
     loaders: [
       {
         test: /\.scss$/,
         loaders: ['style', 'css', 'sass'],
-        include: [
-          path.join(__dirname, 'src'),
-        ],
+        include: [path.join(__dirname, 'src')],
+      },
+      {
+        test: /\.html$/,
+        loader: "html"
       }
     ]
   },
   resolve: {
-    extensions: ['', '.scss', '.css']
+    extensions: ['', '.scss', '.css', '.html', '.htm']
   }
 };
